6e74d0ea987d675635922de62eeb0cc29e8c40ad
moliware <miguel@moliware.com>
2015-11-29 11:17:26 +0100

    Exercise 4.3

--- exercises/src/main/scala/fpinscala/errorhandling/Option.scala
+++ exercises/src/main/scala/fpinscala/errorhandling/Option.scala
@@ -48,7 +48,7 @@ object Option {
     mean(xs.map(x => Math.pow(x - m, 2)))
   }
 
-  def map2[A,B,C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] = sys.error("todo")
+  def map2[A,B,C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] = a.flatMap(x => b.map(y => f(x, y)))
 
   def sequence[A](a: List[Option[A]]): Option[List[A]] = sys.error("todo")
 
